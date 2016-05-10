d8bfe1a56e8899cd93bcafe6e481d28b8b00c600
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-05-10 22:15:40 +1000

    chores: Add dist build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,25 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-      bowerRequirejs: {
-        target: {
-          rjsConfig: 'src/js/config.js'
-        }
-      }
+        clean: {
+            src: ['dist'],
+        },
+        copy: {
+            files: {
+                expand: true,
+                cwd: 'src',
+                src: ['**'],
+                dest: 'dist',
+            },
+        },
+        bowerRequirejs: {
+            target: {
+                rjsConfig: 'src/js/config.js'
+            }
+        },
     });
     grunt.loadNpmTasks('grunt-bower-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['bowerRequirejs']);
+    grunt.registerTask('default', ['clean', 'copy', 'bowerRequirejs']);
 }
