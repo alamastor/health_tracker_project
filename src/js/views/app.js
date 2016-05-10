3900e5f1457c16c786e63ec81f71b7497c09dbc0
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-05-10 23:14:42 +1000

    feat: Working search function

--- src/js/views/app.js
+++ src/js/views/app.js
@@ -1,8 +1,28 @@
-define(['backbone'], function(Backbone) {
+define(['backbone', 'jquery', 'tokens'], function(Backbone, $, tokens) {
     var AppView = Backbone.View.extend({
 
-        search: function() {
+        search: function(searchText) {
             console.log('executing search');
+            $.ajax('https://api.nutritionix.com/v1_1/search/' + searchText, {
+                    data: {
+                        results: '0:20',
+                        cal_min: '0',
+                        cal_max: '50000',
+                        fields: '*',
+                        appId: tokens.nutritionix.id,
+                        appKey: tokens.nutritionix.key,
+                    }
+            }).done(function(data, textStatus, jqXHR) {
+                console.log('search done');
+                console.log(data);
+                console.log(textStatus);
+                console.log(jqXHR);
+            }).fail(function(textStatus, jqXHR, errorThrown) {
+                console.log('search fail');
+                console.log(textStatus);
+                console.log(jqXHR);
+                console.log(errorThrown);
+            });
         }
     });
     return new AppView();
