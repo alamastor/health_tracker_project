459f7b6bf8b0451b92eed58f8f23244ee4af0ed9
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-07-04 20:58:09 +1000

    chore: Fix routes to remove dist path

--- src/js/router.js
+++ src/js/router.js
@@ -3,8 +3,8 @@ var FoodHistory = require('./collections/food_history.js');
 var tokens = require('./tokens.js');
 var Router = Backbone.Router.extend({
     routes: {
-        'dist/': 'exampleCollection',
-        'dist/:uid': 'userCollection'
+        '': 'exampleCollection',
+        '/:uid': 'userCollection'
     },
 });
 
