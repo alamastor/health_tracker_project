1eb71c110ee596fb17a477bb692c82a294d34c87
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-07-16 22:25:13 +1000

    chore: Add tokens

---
+++ src/js/tokens.js
@@ -0,0 +1,15 @@
+'use strict';
+var tokens = {
+    nutritionix: {
+        id: '21f75ade',
+        key: 'e01a63a7b8e2dba4b187ce386582689e',
+    },
+    firebase: {
+        apiKey: 'AIzaSyDk8_N8GOWuQI0nIIfcTf9owgh96bGvwzQ',
+        authDomain: 'udacity-heath-tracker.firebaseapp.com',
+        databaseURL: 'https://udacity-heath-tracker.firebaseio.com',
+        storageBucket: 'udacity-heath-tracker.appspot.com',
+    },
+};
+
+module.exports = tokens;
