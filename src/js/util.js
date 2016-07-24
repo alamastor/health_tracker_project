bf5f9f897fc9e5521a4d50ae5aa6a517f0b0e9f7
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-07-24 17:47:18 +1000

    fix: Clea up lines

--- src/js/util.js
+++ src/js/util.js
@@ -11,7 +11,8 @@ var util = {
     isThisWeek: function(date) {
         var now = new Date();
         var todayDay = now.getDay();
-        var dateDiff = (now.setHours(0,0,0,0) - date.setHours(0,0,0,0)) / (1000*60*60*24);
+        var dateDiff = now.setHours(0,0,0,0) - date.setHours(0,0,0,0);
+        dateDiff = dateDiff / (1000*60*60*24);
         if (dateDiff <= now.getDay()) {
             return true;
         } else {
@@ -19,4 +20,5 @@ var util = {
         }
     },
 };
+
 module.exports = util;
